6cb902ac2c1f865d15375c13ff166fad02cd2404
Aqueel ur Rahman Khan <119236505+Rahmaaaan@users.noreply.github.com>
2023-11-28 12:54:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ x |`' __   ,-~^~-.___ ==I==
   |  |  |{   /~\   }    |
  /|\ \__/ \  \_/  /|   /|\
 / | \|  | /`~-_-~'X.\ //| \
-
 ''')
+
 street = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
-print("Your band name could be " + street + " " + pet)
+print(f"Your band name could be {street} {pet}")
