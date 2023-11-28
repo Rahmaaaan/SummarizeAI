29ee2d78aa42478f496714da1b6bfd97eea6718e
Aqueel ur Rahman Khan <119236505+Rahmaaaan@users.noreply.github.com>
2023-11-28 12:51:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Welcome to the Band Name Generator.")
+print('''
+.-.,      ,--. ,--.
+`/|~\     \__/T`--'     .
+x |`' __   ,-~^~-.___ ==I==
+  |  |--| /       \__}  |
+  |  |  |{   /~\   }    |
+ /|\ \__/ \  \_/  /|   /|\
+/ | \|  | /`~-_-~'X.\ //| \
+
+''')
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
